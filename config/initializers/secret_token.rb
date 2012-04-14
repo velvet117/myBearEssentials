65791d078a76e80920e59d1c53a00511b7479afa
Anastasia Ingargiola <aganchenko@gmail.com>
2012-04-13 17:37:50 -0700

    created new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyBearEssentials::Application.config.secret_token = '8f1a199b882e3b6d8446e9ec17c94d21064989b60703ab90dd287e6fefb2c07d38b904429e7a5ecf88bed589ed86c1a20278b7d3144e7494eb48deafe247d9cf'
