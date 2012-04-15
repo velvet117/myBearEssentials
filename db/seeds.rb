04db61b064594eb2be61b75a1a8c214008f41155
Anastasia Ingargiola <aganchenko@gmail.com>
2012-04-14 23:28:51 -0700

    added some seed data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+cs = Department.create(acronym: 'COMSCI', name: 'Computer Science')
+ee = Department.create(acronym: 'EE', name: 'Electrical Engineering')
+
+Course.create(number:170, title: 'Algorithms', department: cs, units: 4, difficulty: 4)
+Course.create(number:160, title: 'User Interface', department: cs, units: 4, difficulty: 2)
+Course.create(number:188, title: 'Aritificial Intelligence', department: cs, units: 4, difficulty: 4)
+Course.create(number:122, title: 'Networking', department: ee, units: 4, difficulty: 2)
